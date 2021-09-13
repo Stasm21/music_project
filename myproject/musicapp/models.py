ed9e134523bdb4a0d59023388e19d97c012a43c3
Stas <stasm21@gmail.com>
2021-09-13 09:14:11 -0700

    added cors headers in settings

--- myproject/musicapp/models.py
+++ myproject/musicapp/models.py
@@ -6,6 +6,5 @@ class Song(models.Model):
     title = models.CharField(max_length=50)
     artist = models.CharField(max_length=50)
     album = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50)
     release_date = models.DateField()
-    genre = models.CharField(max_length=50, null=True)
-    likes = models.IntegerField(default=0)
